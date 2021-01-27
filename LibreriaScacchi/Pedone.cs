de2df9c74a49ec064c090a2ab9874f5556db4762
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2021-01-27 10:57:06 +0100

    Add project files.

---
+++ LibreriaScacchi/Pedone.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibreriaScacchi
+{
+    public class Pedone:Pezzo
+    {
+        public Pedone(Colore schieramento):base(1,schieramento) { }
+
+        public override void Muovi(Cella nuovaPosizione)
+        {
+            if (nuovaPosizione.Lettera==Posizione.Lettera)
+            {
+                int segno = Schieramento == Colore.Bianco ? +1 : -1;
+                int start = Schieramento == Colore.Bianco ? 2 : 7;
+                if ()
+            }
+        }
+    }
+}
---
+++ LibreriaScacchi/Pezzo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibreriaScacchi
+{
+    public abstract class Pezzo
+    {
+        private Cella posizione;
+        public int Valore { get; private set; }
+        public Colore Schieramento { get; private set; }
+        public Cella Posizione
+        {
+            get => posizione;set
+            {
+                if (posizione != null)
+                    posizione.Pezzo = null;
+                posizione = value;
+                posizione.Pezzo = this;
+            }
+        }
+
+        public string Name => GetType().Name;
+        public virtual string ShortName => Name.Substring(0, 3);
+
+        protected Pezzo (int valore, Colore schieramento)
+        {
+            Valore = valore;
+            Schieramento = schieramento;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} {Schieramento} in {Posizione}";
+        }
+
+        protected void Errore()
+        {
+            throw new Exception($"Mossa per {Name} non valida");
+        }
+    }
+}
